1385c92a4a1df377212b42d66e7171ef6cd09319
Triplanetary <Triplanetary@users.noreply.github.com>
2015-12-10 12:08:13 -0500

    submission.is_self

--- RedditSubmissionArchiver.py
+++ RedditSubmissionArchiver.py
@@ -14,15 +14,15 @@ backlogOfSubmissions = 5 #The number of submissions that the bot should work thr
 reddit.refresh_access_information()
 subreddit = reddit.get_subreddit(subredditName)
 submissionStream = praw.helpers.submission_stream(reddit_session = reddit,
-												  subreddit = subreddit,
-												  limit = backlogOfSubmissions,
-												  verbosity = 0)
+                                                  subreddit = subreddit,
+                                                  limit = backlogOfSubmissions,
+                                                  verbosity = 0)
 
 for submission in submissionStream:
-	print submission.url
-	if submission.url != submission.permalink: #We ignore self-posts
-		urlOfArchive = URLArchiver.archive(submission.url)
-		if urlOfArchive is not None:#Ensure we received a url to archive
-			commentText = "The link submitted with this post has been archived here: " + urlOfArchive
-			#print commentText #Uncomment this line for testing
-			submission.comment(commentText) #Comment out this line for testing
+    print(submission.url)
+    if not submission.is_self: #We ignore self-posts
+        urlOfArchive = URLArchiver.archive(submission.url)
+        if urlOfArchive is not None:#Ensure we received a url to archive
+            commentText = "The link submitted with this post has been archived here: " + urlOfArchive
+            #print commentText #Uncomment this line for testing
+            submission.comment(commentText) #Comment out this line for testing
