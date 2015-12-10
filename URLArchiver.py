347c4ad703a1ab7bca36b9da72dd62fa2cbb3579
Triplanetary <Triplanetary@users.noreply.github.com>
2015-12-10 12:00:55 -0500

    Changed mechanize to requests

--- URLArchiver.py
+++ URLArchiver.py
@@ -1,24 +1,21 @@
 #Imports
-import mechanize
+import requests
 
 #Properties
-baseURL = "https://www.archive.is/"
-browser = mechanize.Browser()
+baseURL = "https://www.archive.is/submit/"
 allowedAttempts = 3
+
 
 #Functions
 def archive(urlToArchive):
-	attempt = 0
-	returnValue = None
-	while attempt < allowedAttempts:
-		try: 
-			browser.open(baseURL)	
-			browser.select_form(nr=0)
-			browser.form["url"] = urlToArchive
-
-			req = browser.submit()
-			returnValue = req.geturl()
-			break
-		except: 
-			attempt += 1
-	return returnValue
+    attempt = 0
+    returnValue = None
+    while attempt < allowedAttempts:
+        try:
+            req = requests.post(baseURL, data={'url': urlToArchive}, timeout=60)
+            if req.status_code == 200:
+                returnValue = req.url
+                break
+        except:
+            attempt += 1
+    return returnValue
