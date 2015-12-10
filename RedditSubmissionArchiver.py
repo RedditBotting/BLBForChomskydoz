cde13ffb47d5fedd1ab38eda1efd0d086df73bf7
Nciacrkson <redditbotting@gmail.com>
2015-12-10 10:15:57 -0500

    -Initial commit, created the RedditSubmissionArchiver and the URLArchiver.

---
+++ RedditSubmissionArchiver.py
@@ -0,0 +1,28 @@
+#imports
+import praw
+import URLArchiver
+
+#Properties
+subredditName = "badlinguistics"
+userAgent = "BadLinguisticsBot2: v0.1 by /u/nciacrkson"
+reddit = praw.Reddit(userAgent)
+backlogOfSubmissions = 5 #The number of submissions that the bot should work through, set to at least 1
+
+#Functions
+
+#Script
+reddit.login() #TODO: Remove use of login() for better automation and because it will be deprecated soon
+subreddit = reddit.get_subreddit(subredditName)
+submissionStream = praw.helpers.submission_stream(reddit_session = reddit,
+												  subreddit = subreddit,
+												  limit = backlogOfSubmissions,
+												  verbosity = 0)
+
+for submission in submissionStream:
+	print submission.url
+	if submission.url != submission.permalink: #We ignore self-posts
+		urlOfArchive = URLArchiver.archive(submission.url)
+		if urlOfArchive is not None:#Ensure we received a url to archive
+			commentText = "The link submitted with this post has been archived here: " + urlOfArchive
+			#print commentText #Uncomment this line for testing
+			submission.comment(commentText) #Comment out this line for testing
